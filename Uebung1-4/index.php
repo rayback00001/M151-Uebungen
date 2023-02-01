f63a3e4a35f1b7d9b149b9464b25199fa5aeb4c7
Rakul <rabrosrajgopu@gmail.com>
2023-02-01 16:10:40 +0100

    created task 4 and done a task

---
+++ Uebung1-4/index.php
@@ -0,0 +1,18 @@
+<?php
+    $username = $_GET['username'];
+    
+    echo "Hallo {$username}!<br />";
+
+    if ($_GET['age']) {
+        $age = $_GET['age'];
+        echo "Du bist {$age} Jahre alt.";
+    }
+
+    // Lösung: http://m151.test/Uebung1-4/index.php?username=raj&age=4
+?>
+
+
+
+   
+
+
