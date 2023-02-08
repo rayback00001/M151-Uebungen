add42d05a35ebeb3b27bb6979d0b12524ca5d0c6
Rakul <rabrosrajgopu@gmail.com>
2023-02-08 13:35:25 +0100

    created new file

---
+++ Uebung2-1/index.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "northwind";
+
+$conn = mysqli_connect($servername, $username, $password);
+
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully<br />";
+
+mysqli_select_db($conn, $database);
+
+echo "Datenbank ausgewählt!<br />";
+?>
