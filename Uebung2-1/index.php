145d24067c0efc9dce19a62d4b1fe00018351327
Rakul <rabrosrajgopu@gmail.com>
2023-02-08 14:05:27 +0100

    finished task

--- Uebung2-1/index.php
+++ Uebung2-1/index.php
@@ -15,4 +15,20 @@ echo "Connected successfully<br />";
 mysqli_select_db($conn, $database);
 
 echo "Datenbank ausgewählt!<br />";
+
+$sql = "select * from customers where job_title = 'Purchasing Representative' ";
+$result = $conn->query($sql);
+
+while($record = mysqli_fetch_assoc($result)){
+   d($record);
+}
+
+function d($args){
+   echo "<pre>";
+   var_dump($args);
+   echo "</pre>";
+}
+
+
+mysqli_close($conn)
 ?>
