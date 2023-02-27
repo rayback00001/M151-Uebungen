4165c2a85c253853ada26b2b09f5f5b6061130a5
Rakul <rabrosrajgopu@gmail.com>
2023-02-27 21:38:21 +0100

    delete

--- Uebung2-3/delete.php
+++ Uebung2-3/delete.php
@@ -1,6 +1,22 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "northwind";
 
-echo 'Tschau bre';
+try {
+  $conn = new PDO("mysql:host=$servername; dbname=$database", $username, $password); // Er definiert, dass der Datenbanktyp mysql ist
+
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Attribute
+  //echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+$delete = $_GET['id'];
+echo '<h2>Bestellung löschen</h2>';
+//$statement = $conn->prepare("delete from orders where orders.customer_id = :id ");
+$statement->execute([':id' => $delete]);
 
 
 ?>
