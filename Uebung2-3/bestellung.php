2e988df7bb2457c8f8156c450dd46852f6a34807
Rakul <rabrosrajgopu@gmail.com>
2023-02-27 15:50:20 +0100

    added css

--- Uebung2-3/bestellung.php
+++ Uebung2-3/bestellung.php
@@ -19,20 +19,24 @@ $statement = $conn->prepare("SELECT * from orders where orders.customer_id = :id
 $statement->execute([':id' => $order]);
 
 
-
 ?>
 
 <table>
+  <thead>
     <tr>
         <th>Bestelldatum</th>
         <th>Ship</th>
         <th>Payment type</th>
         <th>Paid date</th> 
     </tr>
+  </thead>
 
-    <?php  while ($row = $statement->fetch()){
+    <?php  
+    
+    while ($row = $statement->fetch()){
         
         echo " 
+
         <tr>
             <th>{$row['order_date']}</th>
             <th>{$row['ship_name']}</th>
@@ -48,3 +52,31 @@ $statement->execute([':id' => $order]);
     
 
 </table>
+
+
+<style>
+ *{
+   padding: 0;
+   font-family: 'Poppins', sans-serif;
+   margin: 0;
+ }
+ table, th, td {
+   border:1px solid black;
+ }
+ table {
+   border-collapse: collapse;
+   margin: 25px 0;
+   font-size: 0.9em;
+   font-family: sans-serif;
+   min-width: 400px;
+ }
+ table thead tr {
+   background-color: #1E90FF;
+   color: #ffffff;
+   text-align: left;
+ }
+ table th,
+ table td {
+   padding: 12px 15px;
+ }
+</style>
--- Uebung2-3/index.php
+++ Uebung2-3/index.php
@@ -20,28 +20,62 @@ $statement->execute();
 ?>
 
 <table>
+  <thead>
     <tr>
         <th>Vorname</th>
         <th>Nachname</th>
         <th>Jobtitel</th>
+        <th>Adresse</th>
+        <th>Stadt</th>
     </tr>
+  </thead>
 
-    <?php  while ($row = $statement->fetch()){
+    <?php  
+    
+    while ($row = $statement->fetch()){
 
         echo " 
         <tr>
             <th>{$row['first_name']}</th>
             <th>{$row['last_name']}</th>
             <th>{$row['job_title']}</th>
+            <th>{$row['address']}</th>
+            <th>{$row['city']}</th>
             <th><a href='bestellung.php?id={$row['id']}'>Bestellungen</a></th>
         </tr>
         ";
-  
-       
+   
     }
     
     ?>
     
 
 </table>
+
+<style>
+ *{
+   padding: 0;
+   font-family: 'Poppins', sans-serif;
+   margin: 0;
+ }
+ table, th, td {
+   border:1px solid black;
+ }
+ table {
+   border-collapse: collapse;
+   margin: 25px 0;
+   font-size: 0.9em;
+   font-family: sans-serif;
+   min-width: 400px;
+ }
+ table thead tr {
+   background-color: #1E90FF;
+   color: #ffffff;
+   text-align: left;
+ }
+ table th,
+ table td {
+   padding: 12px 15px;
+ }
+</style>
 
